1d8608ed8da8381aca119630dff4f1c8a392adb7
Gustavo Jesus Palmentieri <gustavoo.palmentieri@gmail.com>
2026-03-03 21:13:44 -0300

    Add files via upload

---
+++ Java - Gustavo/Aula02/src/App.java
@@ -0,0 +1,7 @@
+/*AQUI ESTÁ O CÓDIGO FONTE*/
+
+public class App { //CLASSE PÚBLICA - ESTÁ VÍSIVEL PARA OS OBJETOS
+    public static void main(String[] args) { //FUNÇÃO PRINCIPAL
+        System.out.println("Boa noite!!");
+    }
+}
