2df57311e53fd6295db3e1a504596a04a41aac31
Gustavo Jesus Palmentieri <gustavoo.palmentieri@gmail.com>
2026-05-19 21:32:17 -0300

    Add files via upload

--- Java - Gustavo/Aula08 - 28-04/src/App.java
+++ Java - Gustavo/Aula08 - 28-04/src/App.java
@@ -5,18 +5,100 @@ public class App {
         Scanner sc = new Scanner(System.in);
         Aluno aluno1 = new Aluno();
 
-        //APLICANDO SCANNER 
+        int opcao = 0; //OPÇÕES DO MENU
 
-        System.out.printf("\nDigite a nota: ");
-        float nota = sc.nextFloat();
-        aluno1.aplicar_nota(nota);
+        do{
+            
+            System.out.printf("\n#####--- MENU ---#####\n");
+            System.out.printf("\n--- > 1 - APLICAR NOTA < ---");
+            System.out.printf("\n--- > 2 - APLICAR PRESENÇA < ---");
+            System.out.printf("\n--- > 3 - APLICAR BÔNUS < ---");
+            System.out.printf("\n--- > 4 - EXIBIR DADOS < ---\n");
+            System.out.printf("\n--- > 5 - SAIR < ---\n");
+            System.out.printf("\nDigite a Opção: ");
 
-        System.out.printf("\nDigite o total de presenças: ");
-        int presencas = sc.nextInt();
-        aluno1.aplicar_presencas(presencas);
+            try{
 
-        System.out.printf("\nDigite o bônus: ");
-        int bonus = sc.nextInt();
-        aluno1.aplicar_bonus(bonus);
+                opcao = sc.nextInt();
+                
+            switch(opcao){
+
+                case (1): 
+
+                    try{ //TRATAMENTO DE EXCEÇÕES 
+
+                        System.out.printf("\nDigite a nota: ");
+                        float nota = sc.nextFloat();
+                        aluno1.aplicar_nota(nota);
+                    }
+
+                    catch(Exception e){
+
+                        System.out.printf("\nValor Incorreto!\n");
+                        sc.nextLine(); //É O COMANDO QUE VAI ESVAZIAR O SCANNER - PARA UTILIZAR ESTE METÓDO TEM QUE ESTAR DENTRO DO "CATCH"
+
+                    }
+
+                    break;
+
+                case (2):
+
+                    try{
+
+                        System.out.printf("\nDigite o total de presenças: ");
+                        int presencas = sc.nextInt();
+                        aluno1.aplicar_presencas(presencas);
+                    }
+        
+                    catch(Exception e){
+        
+                        System.out.printf("\nValor Incorreto!!\n");
+                        sc.nextLine();
+                    }
+
+                    break;
+
+                case (3):
+
+                    try{
+
+                        System.out.printf("\nDigite o bônus: ");
+                        int bonus = sc.nextInt();
+                        aluno1.aplicar_bonus(bonus);
+                    }
+        
+                    catch(Exception e){
+        
+                        System.out.printf("\nValor Incorreto\n");
+                        sc.nextLine();
+                    }
+
+                    break;
+
+                case (4):
+
+                    aluno1.exibir_dados();
+                    break;
+
+                case (5):
+
+                    System.out.printf("\nUsuário saiu do programa!!");
+                    break;
+                
+                default: //SIGNIFICA PADRÃO
+                
+                    System.out.printf("\nOpção Inexistente!");
+            }
+                } 
+                
+                catch(Exception e){
+                    
+                    System.out.printf("\nDigte apenas números\n");
+                    sc.nextLine();
+
+                }
+
+        } while (opcao != 5);
+
     }
 }
--- Java - Gustavo/Aula08 - 28-04/src/Pessoa.java
+++ Java - Gustavo/Aula08 - 28-04/src/Pessoa.java
@@ -2,15 +2,17 @@ public class Pessoa {
 
     float nota;
     int presencas;
-
+    float nota_final;
+    float bonus;
 
     //METÓDO APLICAR NOTA:
 
-    public void aplicar_nota(float nota){
+    public void aplicar_nota(float nota){ //"FLOAT NOTA" - É O PÂRAMETRO DO METÓDO
 
-        if (nota >= 0){
+        if (nota >= 0 && nota <= 10){
 
-            this.nota = nota; //"THIS" - NESSA LINHA SIGNIFICA A NOTA DO ATRIBUTO
+            this.nota = nota; //"THIS" - NESSA LINHA SIGNIFICA A NOTA DO ATRIBUTO - DIFERENCIANDO DO PARÂMETRO DO METÓDO
+            nota_final = this.nota;
             System.out.printf("\nOperação Realizada!!! Nota: %.1f.\n", this.nota);
         }
         
@@ -25,7 +27,7 @@ public class Pessoa {
 
     public void aplicar_presencas(int presencas){
 
-        if (presencas > 0){
+        if (presencas > 0 && presencas <= 40){
 
             this.presencas = presencas;
             System.out.printf("\nOperação Realizada!!! Total de Presenças: %d\n", this.presencas);
@@ -39,6 +41,7 @@ public class Pessoa {
         }
 
     }
+
 }
 
 class Aluno extends Pessoa{
@@ -47,11 +50,12 @@ class Aluno extends Pessoa{
 
     public void aplicar_bonus(float bonus){
 
-        if (presencas > 20){
+        if (presencas > 20 && bonus <= 10){
 
             bonus = bonus / 100; //TRANSFORMANDO EM DECIMAL
-            nota += nota * bonus; //ACRESCENTANDO 
-            System.out.printf("\nOperação Realizada!!! Nota Final (Nota + Bônus): %.1f\n", nota);
+            nota_final += nota_final * bonus; //ACRESCENTANDO
+            this.bonus = bonus; 
+            System.out.printf("\nOperação Realizada!!! Nota Final (Nota + Bônus): %.1f\n", nota_final);
 
         }
 
@@ -59,6 +63,17 @@ class Aluno extends Pessoa{
 
             System.out.printf("\nOperação Não Realizada!!! Nota Final: %.1f\n", nota);
         }
+    }
+
+    //METÓDO EXIBIR DADOS:
+
+    public void exibir_dados(){
+
+        System.out.printf("\nNota Inicial: %.1f", nota);
+        System.out.printf("\nNota Final: %.1f", nota_final);
+        System.out.printf("\nPresenças: %d", presencas);
+        System.out.printf("\nBonûs: %.0f%%", bonus * 100);
+
     }
 
 }
